0f2158b69a6e056fe89c2b29de260e2c61633726
Guilherme Veiga <58952515+guilherme-dsveiga@users.noreply.github.com>
2021-11-10 05:00:25 -0300

    Revertendo mudanças

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "-",
-        authDomain: "-",
-        projectId: "-",
-        storageBucket: "-",
-        messagingSenderId: "-",
-        appId: "-"
+        apiKey: "AIzaSyBr0xIin0Ce-Gwxf-EW_4J98BLZT9dgkzo",
+        authDomain: "reactfire-chat-de65f.firebaseapp.com",
+        projectId: "reactfire-chat-de65f",
+        storageBucket: "reactfire-chat-de65f.appspot.com",
+        messagingSenderId: "135641623297",
+        appId: "1:135641623297:web:568a7b1c7d9775c44fc900"
 })
 
 const db = firebaseApp.firestore();
